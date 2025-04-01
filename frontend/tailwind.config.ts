b5460a4e1a7b52357259464dceda1bdfb96ff987
nico-private-dev <nforget82@gmail.com>
2025-04-01 15:26:26 +0200

    Reinstall tailwind refs: #44

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,18 @@
+module.exports = {
+    important: true,
+    theme: {
+      fontFamily: {
+
+      },
+      extend: {
+        colors: {
+          primary: '#000091',
+          secondary: '#1212FF',
+          tertiary: '#F5F5FE',
+          grey: '#F6F6F6',
+          black: '#000000',
+          white: '#FFFFFF',
+        }
+      }
+    }
+  }
